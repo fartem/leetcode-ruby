e2629489142061d3b25414b6ea877c6156fbea25
fartem <jaman.smlnsk@gmail.com>
2023-04-24 09:06:45 +0300

    2023-04-24 v. 0.0.7: added "21. Merge Two Sorted Lists"

---
+++ lib/easy/21_merge_two_sorted_lists.rb
@@ -0,0 +1,28 @@
+require_relative '../common/linked_list'
+
+# @param {ListNode} list1
+# @param {ListNode} list2
+# @return {ListNode}
+def merge_two_lists(list1, list2)
+  return list1 if list2.nil?
+  return list2 if list1.nil?
+
+  min = list1.val > list2.val ? list2 : list1
+  max = list1.val > list2.val ? list1 : list2
+  result = ListNode.new(min.val)
+  min = min.next
+  head = result
+  while !min.nil? && !max.nil?
+    if min.val > max.val
+      result.next = ListNode.new(max.val)
+      max = max.next
+    else
+      result.next = ListNode.new(min.val)
+      min = min.next
+    end
+    result = result.next
+  end
+  result.next = min unless min.nil?
+  result.next = max unless max.nil?
+  head
+end
