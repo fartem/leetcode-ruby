7f249623c911d7daf3a40a736c1c4b5c13c5ba99
fartem <jaman.smlnsk@gmail.com>
2023-04-26 07:01:37 +0300

    2023-04-26 v. 0.0.9.1: updated tests for "26. Remove Duplicates from Sorted Array"

--- test/easy/test_26_remove_duplicates_from_sorted_array.rb
+++ test/easy/test_26_remove_duplicates_from_sorted_array.rb
@@ -3,7 +3,12 @@ require 'minitest/autorun'
 
 class RemoveDuplicatesFromSortedArrayTest < Minitest::Test
   def test_default
-    assert remove_duplicates([1, 1, 2]) == 2
-    assert remove_duplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]) == 5
+    arr = [1, 1, 2]
+    assert remove_duplicates(arr) == 2
+    assert arr.take(2) == [1, 2]
+
+    arr = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+    assert remove_duplicates(arr) == 5
+    arr.take(5) == [0, 1, 2, 3, 4]
   end
 end
