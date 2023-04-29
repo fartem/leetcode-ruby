61662dd4d2c978bb6c7940449cc46797069f8a63
fartem <jaman.smlnsk@gmail.com>
2023-04-29 20:38:26 +0300

    2023-04-29 v. 0.1.4: added "67. Add Binary"

---
+++ test/easy/test_67_add_binary.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/easy/67_add_binary'
+require 'minitest/autorun'
+
+class AddBinaryTest < ::Minitest::Test
+  def test_default
+    assert_equal('100', add_binary('11', '1'))
+    assert_equal('10101', add_binary('1010', '1011'))
+  end
+end
