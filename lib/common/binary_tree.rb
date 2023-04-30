383af1c1da3bbc0822bc67eb27b014fc23696da7
fartem <jaman.smlnsk@gmail.com>
2023-04-30 16:57:33 +0300

    2023-04-30 v. 0.1.9: added "94. Binary Tree Inorder Traversal"

---
+++ lib/common/binary_tree.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# Binary Tree implementation for this project
+class TreeNode
+  # @param val {Integer}
+  # @param left {TreeNode}
+  # @param right {TreeNode}
+  def initialize(val = 0, left = nil, right = nil)
+    @val = val
+    @left = left
+    @right = right
+  end
+
+  # @param other {TreeNode}
+  def ==(other)
+    are_equals(self, other)
+  end
+
+  # @param curr {TreeNode}
+  # @param other {TreeNode}
+  def are_equals(curr, other)
+    return true if curr.nil? && other.nil?
+    return false if curr.nil? || other.nil?
+
+    curr_eq = curr.val == other.val
+    left_eq = are_equals(curr.left, other.left)
+    right_eq = are_equals(curr.right, other.right)
+    curr_eq && left_eq && right_eq
+  end
+
+  attr_accessor :val, :left, :right
+end
---
+++ lib/easy/94_binary_tree_inorder_traversal.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# @param {TreeNode} root
+# @return {Integer[]}
+def inorder_traversal(root)
+  result = []
+  traverse(root, result)
+  result
+end
+
+# @param {TreeNode} node
+# @param {Integer[]} values
+def traverse(node, values)
+  return if node.nil?
+
+  traverse(node.left, values)
+  values << node.val
+  traverse(node.right, values)
+end
