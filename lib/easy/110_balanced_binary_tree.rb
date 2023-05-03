7eef7764c9898b6810e5e913231f1c4f6da560ea
fartem <jaman.smlnsk@gmail.com>
2023-05-03 07:16:00 +0300

    2023-05-03 v. 0.2.4: added "110. Balanced Binary Tree"

---
+++ lib/easy/110_balanced_binary_tree.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# @param {TreeNode} root
+# @return {Boolean}
+def is_balanced(root)
+  return true if root.nil?
+
+  left_height = max_height(root.left)
+  right_height = max_height(root.right)
+
+  left = is_balanced(root.left)
+  right = is_balanced(root.right)
+
+  (left_height - right_height).abs < 2 && left && right
+end
+
+# @param {TreeNode} root
+# @return {Integer}
+def max_height(node)
+  return 0 if node.nil?
+
+  [max_height(node.left), max_height(node.right)].max + 1
+end
