a01287d3326e5da489337ab91e32c061338bcc83
fartem <jaman.smlnsk@gmail.com>
2023-05-04 22:13:03 +0300

    2023-05-04 v. 0.2.8: added "119. Pascal's Triangle II"

---
+++ test/easy/test_119_pascals_triangle_ii.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/easy/119_pascals_triangle_ii'
+require 'minitest/autorun'
+
+class PascalsTriangleIITest < ::Minitest::Test
+  def test_default
+    assert_equal([1, 3, 3, 1], get_row(3))
+    assert_equal([1], get_row(0))
+    assert_equal([1, 1], get_row(1))
+  end
+end
