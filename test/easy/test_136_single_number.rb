a87fa114c144264181f70f70ba00dfcaed9f8fe1
fartem <jaman.smlnsk@gmail.com>
2023-05-06 07:18:25 +0300

    2023-05-06 v. 0.3.1: added "136. Single Number"

---
+++ test/easy/test_136_single_number.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/easy/136_single_number'
+require 'minitest/autorun'
+
+class SingleNumberTest < ::Minitest::Test
+  def test_default
+    assert_equal(1, single_number([2, 2, 1]))
+    assert_equal(4, single_number([4, 1, 2, 1, 2]))
+    assert_equal(1, single_number([1]))
+  end
+end
