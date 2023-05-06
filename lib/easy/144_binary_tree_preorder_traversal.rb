bb7311d72c6f45dc3590c3dd43a8ff80b18a2e42
fartem <jaman.smlnsk@gmail.com>
2023-05-06 20:59:19 +0300

    2023-05-07 v. 0.3.0: added "144. Binary Tree Preorder Traversal"

---
+++ lib/easy/144_binary_tree_preorder_traversal.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# @param {TreeNode} root
+# @return {Integer[]}
+def preorder_traversal(root)
+  result = []
+  preorder_traverse(root, result)
+  result
+end
+
+# @param {TreeNode} node
+# @param {Integer[]} values
+def preorder_traverse(node, values)
+  return if node.nil?
+
+  values << node.val
+  preorder_traverse(node.left, values)
+  preorder_traverse(node.right, values)
+end
