0424c04f319514b4d64e08cd8b132c8a406397d1
fartem <jaman.smlnsk@gmail.com>
2023-05-10 06:46:10 +0300

    2023-05-10 v. 0.4.2.3: updated tests

--- test/easy/test_203_remove_linked_list_elements.rb
+++ test/easy/test_203_remove_linked_list_elements.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative '../test_helper'
 require_relative '../../lib/easy/203_remove_linked_list_elements'
 require_relative '../../lib/common/linked_list'
 require 'minitest/autorun'
