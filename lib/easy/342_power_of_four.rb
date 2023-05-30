b1ce0c0630a401fa6a2d52fc93b6b6e70f5c8e02
fartem <jaman.smlnsk@gmail.com>
2023-05-30 06:18:53 +0300

    2023-05-30 v. 0.6.5: added "342. Power of Four"

---
+++ lib/easy/342_power_of_four.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/power-of-four/description/
+# @param {Integer} n
+# @return {Boolean}
+def is_power_of_four(n)
+  return false if n.negative?
+
+  num = n / 4
+  pow = 0
+  until num.zero?
+    num /= 4
+    pow += 1
+  end
+
+  4.pow(pow) == n
+end
