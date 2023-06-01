ff76babd8aea85b91298b231c97bca24c9a73d00
fartem <jaman.smlnsk@gmail.com>
2023-06-01 06:16:35 +0300

    2023-06-01 v. 0.6.8: added "349. Intersection of Two Arrays"

---
+++ lib/easy/349_intersection_of_two_arrays.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/intersection-of-two-arrays/
+# @param {Integer[]} nums1
+# @param {Integer[]} nums2
+# @return {Integer[]}
+def intersection(nums1, nums2)
+  result = []
+  nums1_s = nums1.to_set
+  nums2_s = nums2.to_set
+
+  nums1_s.each do |num|
+    result << num if nums2_s.include?(num)
+  end
+
+  result
+end
