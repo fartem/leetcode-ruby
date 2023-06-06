ca67961b962560a99000ceb1ca5fef9afbba8776
fartem <jaman.smlnsk@gmail.com>
2023-06-06 07:01:06 +0300

    2023-06-06 v. 0.7.4: added "389. Find the Difference"

---
+++ test/easy/test_389_find_the_difference.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/easy/389_find_the_difference'
+require 'minitest/autorun'
+
+class FindTheDifferenceTest < ::Minitest::Test
+  def test_default
+    assert_equal(
+      'e',
+      find_the_difference(
+        'abcd',
+        'abcde'
+      )
+    )
+    assert_equal(
+      'y',
+      find_the_difference('', 'y')
+    )
+  end
+end
