92e09f332af08cb1ebb1dabfbae58cecd8ec880b
fartem <jaman.smlnsk@gmail.com>
2023-06-17 20:48:07 +0300

    2023-06-17 v. 0.8.7.4: added version checker

---
+++ ci/version_checker.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rubygems'
+require 'open-uri'
+
+# Process version checker
+def process
+  local_spec = ::Gem::Specification.load('leetcode-ruby.gemspec')
+
+  response = ::URI.open('https://raw.githubusercontent.com/fartem/leetcode-ruby/master/leetcode-ruby.gemspec').read
+  ::File.write('leetcode-ruby.gemspec.remote', response)
+  remote_spec = ::Gem::Specification.load('leetcode-ruby.gemspec.remote')
+
+  exit(1) if local_spec.version <= remote_spec.version
+end
+
+process
