fcb9c9531d28e0c1423009e9ae46c5087e8c72e1
fartem <jaman.smlnsk@gmail.com>
2023-06-22 07:22:09 +0300

    2023-06-22 v. 0.9.2: added "482. License Key Formatting"

---
+++ test/easy/test_482_license_key_formatting.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/easy/482_license_key_formatting'
+require 'minitest/autorun'
+
+class LicenseKeyFormattingTest < ::Minitest::Test
+  def test_default
+    assert_equal(
+      '5F3Z-2E9W',
+      license_key_formatting(
+        '5F3Z-2e-9-w',
+        4
+      )
+    )
+    assert_equal(
+      '2-5G-3J',
+      license_key_formatting(
+        '2-5g-3-J',
+        2
+      )
+    )
+  end
+end
