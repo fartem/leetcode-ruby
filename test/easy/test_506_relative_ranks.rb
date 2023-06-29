864109d87bc078c96b999890474301a715eb137f
fartem <jaman.smlnsk@gmail.com>
2023-06-29 05:44:23 +0300

    2023-06-29 v. 0.9.9: added "506. Relative Ranks"

---
+++ test/easy/test_506_relative_ranks.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/easy/506_relative_ranks'
+require 'minitest/autorun'
+
+class RelativeRanksTest < ::Minitest::Test
+  def test_default
+    assert_equal(
+      ['Gold Medal', 'Silver Medal', 'Bronze Medal', '4', '5'],
+      find_relative_ranks(
+        [5, 4, 3, 2, 1]
+      )
+    )
+    assert_equal(
+      ['Gold Medal', '5', 'Bronze Medal', 'Silver Medal', '4'],
+      find_relative_ranks(
+        [10, 3, 8, 9, 4]
+      )
+    )
+  end
+end
