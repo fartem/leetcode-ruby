f18a29fa273a8f63d6582f0b5c8f70562abbe376
fartem <jaman.smlnsk@gmail.com>
2023-07-09 09:14:16 +0300

    2023-01-09 v. 1.1.2: updated "455. Assign Cookies"

--- lib/easy/455_assign_cookies.rb
+++ lib/easy/455_assign_cookies.rb
@@ -5,14 +5,14 @@
 # @param {Integer[]} s
 # @return {Integer}
 def find_content_children(g, s)
-  g_s = g.sort
-  s_s = s.sort
+  g.sort!
+  s.sort!
   g_p = 0
   s_p = 0
   result = 0
-  while g_p < g_s.length && s_p < s_s.length
-    s_p += 1 while s_p < s_s.length && s_s[s_p] < g_s[g_p]
-    next unless s_p < s_s.length
+  while g_p < g.length && s_p < s.length
+    s_p += 1 while s_p < s.length && s[s_p] < g[g_p]
+    next unless s_p < s.length
 
     g_p += 1
     s_p += 1
