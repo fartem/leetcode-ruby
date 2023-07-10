3379eff654bc0aed6c524c5e829f7a5d52c0c417
fartem <jaman.smlnsk@gmail.com>
2023-07-10 05:42:44 +0300

    2023-01-10 v. 1.1.4: added "563. Binary Tree Tilt"

---
+++ test/easy/test_563_binary_tree_tilt.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/common/binary_tree'
+require_relative '../../lib/easy/563_binary_tree_tilt'
+require 'minitest/autorun'
+
+class BinaryTreeTiltTest < ::Minitest::Test
+  def test_default
+    assert_equal(
+      1,
+      find_tilt(
+        ::TreeNode.new(
+          1,
+          ::TreeNode.new(2),
+          ::TreeNode.new(3)
+        )
+      )
+    )
+    assert_equal(
+      15,
+      find_tilt(
+        ::TreeNode.new(
+          4,
+          ::TreeNode.new(
+            2,
+            ::TreeNode.new(3),
+            ::TreeNode.new(5)
+          ),
+          ::TreeNode.new(
+            9,
+            nil,
+            ::TreeNode.new(7)
+          )
+        )
+      )
+    )
+    assert_equal(
+      9,
+      find_tilt(
+        ::TreeNode.new(
+          21,
+          ::TreeNode.new(
+            7,
+            ::TreeNode.new(
+              1,
+              ::TreeNode.new(3),
+              ::TreeNode.new(3)
+            ),
+            ::TreeNode.new(1)
+          ),
+          ::TreeNode.new(
+            14,
+            ::TreeNode.new(2),
+            ::TreeNode.new(2)
+          )
+        )
+      )
+    )
+  end
+end
