0dc675dbca4942cd63ced770fe300f2361f381f3
fartem <jaman.smlnsk@gmail.com>
2023-07-11 07:22:39 +0300

    2023-01-11 v. 1.1.5: added "566. Reshape the Matrix"

---
+++ lib/easy/566_reshape_the_matrix.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/reshape-the-matrix/
+# @param {Integer[][]} mat
+# @param {Integer} r
+# @param {Integer} c
+# @return {Integer[][]}
+def matrix_reshape(mat, r, c)
+  return mat if mat.length * mat.first.length != r * c
+
+  k = 0
+  l = 0
+  result = []
+  (0...r).step(1) do |i|
+    result << []
+    (0...c).step(1) do |j|
+      if l == mat.first.length
+        l = 0
+        k += 1
+      end
+      result[i].insert(j, mat[k][l])
+      l += 1
+    end
+  end
+
+  result
+end
