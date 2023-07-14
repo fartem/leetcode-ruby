306fecb0c65852608409051e7b2d1e305bfcacd5
fartem <jaman.smlnsk@gmail.com>
2023-07-14 09:00:26 +0300

    2023-01-14 v. 1.1.8: added "590. N-ary Tree Postorder Traversal"

---
+++ lib/easy/590_n_ary_tree_postorder_traversal.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/n-ary-tree-postorder-traversal/
+# @param {Node} root
+# @return {List[int]}
+def postorder(root)
+  return [] if root.nil?
+
+  result = []
+  stack = []
+  stack.push(root)
+  until stack.empty?
+    node = stack.pop
+    result.insert(0, node.val)
+    node.children&.each { |child| stack.push(child) }
+  end
+
+  result
+end
