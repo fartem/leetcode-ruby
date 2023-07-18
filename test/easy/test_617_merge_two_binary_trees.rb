09f284f051b3870a6ed6708201ba557f9f854e25
fartem <jaman.smlnsk@gmail.com>
2023-07-18 08:05:52 +0300

    2023-01-18 v. 1.2.3: added "617. Merge Two Binary Trees"

---
+++ test/easy/test_617_merge_two_binary_trees.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/common/binary_tree'
+require_relative '../../lib/easy/617_merge_two_binary_trees'
+require 'minitest/autorun'
+
+class MergeTwoBinaryTreesTest < ::Minitest::Test
+  def test_default
+    assert_equal(
+      ::TreeNode.new(
+        3,
+        ::TreeNode.new(
+          4,
+          ::TreeNode.new(5),
+          ::TreeNode.new(4)
+        ),
+        ::TreeNode.new(
+          5,
+          nil,
+          ::TreeNode.new(7)
+        )
+      ),
+      merge_trees(
+        ::TreeNode.new(
+          1,
+          ::TreeNode.new(
+            3,
+            ::TreeNode.new(5),
+            nil
+          ),
+          ::TreeNode.new(2)
+        ),
+        ::TreeNode.new(
+          2,
+          ::TreeNode.new(
+            1,
+            nil,
+            ::TreeNode.new(4)
+          ),
+          ::TreeNode.new(
+            3,
+            nil,
+            ::TreeNode.new(7)
+          )
+        )
+      )
+    )
+    assert_equal(
+      ::TreeNode.new(
+        2,
+        ::TreeNode.new(2),
+        nil
+      ),
+      merge_trees(
+        ::TreeNode.new(1),
+        ::TreeNode.new(
+          1,
+          ::TreeNode.new(2),
+          nil
+        )
+      )
+    )
+  end
+end
