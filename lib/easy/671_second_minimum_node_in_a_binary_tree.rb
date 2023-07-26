02fdfcc3f4c1ab4703a138e3d0aa7b88c290bdcc
fartem <jaman.smlnsk@gmail.com>
2023-07-26 08:08:55 +0300

    2023-01-26 v. 1.3.0: added "671. Second Minimum Node In a Binary Tree"

---
+++ lib/easy/671_second_minimum_node_in_a_binary_tree.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+$max = 1 << (1 << 16)
+$f_min = 0
+$s_min = $max
+
+# https://leetcode.com/problems/second-minimum-node-in-a-binary-tree/
+# @param {TreeNode} root
+# @return {Integer}
+def find_second_minimum_value(root)
+  $f_min = root.val
+  $s_min = $max
+  find_second_minimum(root)
+
+  $s_min < $max ? $s_min : -1
+end
+
+# @param {TreNode} node
+def find_second_minimum(node)
+  return if node.nil?
+
+  if $f_min < node.val && node.val < $s_min
+    $s_min = node.val
+  elsif $f_min == node.val
+    find_second_minimum(node.left)
+    find_second_minimum(node.right)
+  end
+end
---
+++ test/easy/test_671_minimum_node_in_a_binary_tree.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/common/binary_tree'
+require_relative '../../lib/easy/671_second_minimum_node_in_a_binary_tree'
+require 'minitest/autorun'
+
+class RobotReturnToOriginTest < ::Minitest::Test
+  def test_default
+    assert_equal(
+      5,
+      find_second_minimum_value(
+        ::TreeNode.new(
+          2,
+          ::TreeNode.new(2),
+          ::TreeNode.new(
+            5,
+            ::TreeNode.new(5),
+            ::TreeNode.new(7)
+          )
+        )
+      )
+    )
+    assert_equal(
+      -1,
+      find_second_minimum_value(
+        ::TreeNode.new(
+          2,
+          ::TreeNode.new(2),
+          ::TreeNode.new(2)
+        )
+      )
+    )
+  end
+end
