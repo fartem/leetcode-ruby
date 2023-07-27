d1ad8735a87eed9d1d2a210233631435af0d47af
fartem <jaman.smlnsk@gmail.com>
2023-07-27 07:50:12 +0300

    2023-01-27 v. 1.3.1: added "674. Longest Continuous Increasing Subsequence"

---
+++ lib/easy/674_longest_continuous_increasing_subsequence.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+# @param {Integer[]} nums
+# @return {Integer}
+def find_length_of_lcis(nums)
+  result = 0
+  max = 1
+  (1...nums.length).step(1) do |i|
+    if nums[i - 1] < nums[i]
+      max += 1
+    else
+      result = max if max > result
+      max = 1
+    end
+  end
+
+  [result, max].max
+end
---
+++ test/easy/test_674_longest_continuous_increaseing_subsequence.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/easy/674_longest_continuous_increasing_subsequence'
+require 'minitest/autorun'
+
+class LongestContinuousIncreasingSubsequenceTest < ::Minitest::Test
+  def test_default
+    assert_equal(
+      3,
+      find_length_of_lcis(
+        [1, 3, 5, 4, 7]
+      )
+    )
+    assert_equal(
+      1,
+      find_length_of_lcis(
+        [2, 2, 2, 2, 2]
+      )
+    )
+  end
+end
