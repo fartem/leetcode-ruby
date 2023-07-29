e31e69ddd0821a298d2cfd08549ffe9c4409bddf
fartem <jaman.smlnsk@gmail.com>
2023-07-29 10:54:43 +0300

    2023-01-29 v. 1.3.3: added "682. Baseball Game"

---
+++ lib/easy/682_baseball_game.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/baseball-game/
+# @param {String[]} operations
+# @return {Integer}
+def cal_points(operations)
+  stack = []
+  operations.each do |op|
+    case op
+    when '+'
+      a = stack.pop
+      b = stack.pop
+      stack.push(b)
+      stack.push(a)
+      stack.push(a + b)
+    when 'D'
+      stack.push(stack[stack.length - 1] * 2)
+    when 'C'
+      stack.pop
+    else
+      stack.push(op.to_i)
+    end
+  end
+
+  stack.sum
+end
