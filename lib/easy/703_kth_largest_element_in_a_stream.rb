39617e08dd96299cddd66556e4f1ea4f621a09b7
fartem <jaman.smlnsk@gmail.com>
2023-08-01 08:46:24 +0300

    2023-08-01 v. 1.3.6: added "703. Kth Largest Element in a Stream"

---
+++ lib/easy/703_kth_largest_element_in_a_stream.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/kth-largest-element-in-a-stream/
+class KthLargest
+  # @param {Integer} k
+  # @param {Integer[]} nums
+  def initialize(k, nums)
+    @k = k
+    @a = nums.sort.reverse.first(k)
+  end
+
+  # @param {Integer} val
+  # @@return {Integer}
+  def add(val)
+    if @a.size < @k || val > @a.last
+      @a.insert(@a.bsearch_index { |v| val >= v } || -1, val)
+      @a.pop if @a.size > @k
+    end
+
+    @a.last
+  end
+end
---
+++ test/easy/test_703_kth_largest_element_in_a_stream.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/easy/703_kth_largest_element_in_a_stream'
+require 'minitest/autorun'
+
+class KthLargestElementInAStreamTest < ::Minitest::Test
+  def test_default
+    kth_largest = ::KthLargest.new(3, [4, 5, 8, 2])
+
+    assert_equal(4, kth_largest.add(3))
+    assert_equal(5, kth_largest.add(5))
+    assert_equal(5, kth_largest.add(10))
+    assert_equal(8, kth_largest.add(9))
+    assert_equal(8, kth_largest.add(4))
+  end
+end
