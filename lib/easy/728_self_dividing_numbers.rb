b15a2e72e0a3ca51118d150d963af407078b375f
fartem <jaman.smlnsk@gmail.com>
2023-08-08 06:29:17 +0300

    2023-08-08 v. 1.4.3: added "728. Self Dividing Numbers"

---
+++ lib/easy/728_self_dividing_numbers.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/self-dividing-numbers/
+# @param {Integer} left
+# @param {Integer} right
+# @return {Integer[]}
+def self_dividing_numbers(left, right)
+  result = []
+  (left..right).each do |num|
+    cpy = num
+    is_self_dividing = true
+    until cpy.zero?
+      candidate = cpy % 10
+      if candidate.zero?
+        is_self_dividing = false
+        break
+      end
+
+      rem = num % candidate
+      unless rem.zero?
+        is_self_dividing = false
+        break
+      end
+
+      cpy /= 10
+    end
+
+    result << num if is_self_dividing
+  end
+
+  result
+end
