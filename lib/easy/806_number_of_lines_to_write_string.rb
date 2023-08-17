32d114f890538613675752226999f623d4c9be8a
fartem <jaman.smlnsk@gmail.com>
2023-08-17 05:52:10 +0300

    2023-08-17 v. 1.5.4: added "806. Number of Lines To Write String"

---
+++ lib/easy/806_number_of_lines_to_write_string.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/number-of-lines-to-write-string/
+# @param {Integer[]} widths
+# @param {String} s
+# @return {Integer[]}
+def number_of_lines(widths, s)
+  lines = 1
+  width = 0
+  (0...s.length).step(1) do |i|
+    c = s[i]
+    w = widths[c.ord - 97]
+    width += w
+
+    if width > 100
+      lines += 1
+      width = w
+    end
+  end
+
+  [lines, width]
+end
---
+++ test/easy/test_806_number_of_lines_to_write_string.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/easy/806_number_of_lines_to_write_string'
+require 'minitest/autorun'
+
+class NumberOfLinesToWriteStringTest < ::Minitest::Test
+  def test_default
+    assert_equal(
+      [3, 60],
+      number_of_lines(
+        [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+        'abcdefghijklmnopqrstuvwxyz'
+      )
+    )
+    assert_equal(
+      [2, 4],
+      number_of_lines(
+        [4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+        'bbbcccdddaaa'
+      )
+    )
+  end
+end
