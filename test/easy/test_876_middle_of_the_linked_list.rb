edd83111ef65fd9f51e902e90291ff7e00b2d7e2
fartem <jaman.smlnsk@gmail.com>
2023-08-22 07:52:44 +0300

    2023-08-22 v. 1.6.6: added "876. Middle of the Linked List"

---
+++ test/easy/test_876_middle_of_the_linked_list.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/common/linked_list'
+require_relative '../../lib/easy/876_middle_of_the_linked_list'
+require 'minitest/autorun'
+
+class MiddleOfTheLinkedListTest < ::Minitest::Test
+  def test_default
+    assert(
+      ::ListNode.are_equals(
+        ::ListNode.from_array([3, 4, 5]),
+        middle_node(::ListNode.from_array([1, 2, 3, 4, 5]))
+      )
+    )
+    assert(
+      ::ListNode.are_equals(
+        ::ListNode.from_array([4, 5, 6]),
+        middle_node(::ListNode.from_array([1, 2, 3, 4, 5, 6]))
+      )
+    )
+  end
+end
