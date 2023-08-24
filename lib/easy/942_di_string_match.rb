b8b24eb481db45a4062f04e8a50d1b30a5c591aa
fartem <jaman.smlnsk@gmail.com>
2023-08-24 09:24:01 +0300

    2023-08-24 v. 1.7.9: added "942. DI String Match"

---
+++ lib/easy/942_di_string_match.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/di-string-match/
+# @param {String} s
+# @return {Integer[]}
+def di_string_match(s)
+  n = s.length
+  result = ::Array.new(n + 1)
+  l = 0
+  h = n
+  (0...n).step(1) do |i|
+    if s[i] == 'I'
+      result[i] = l
+      l += 1
+    else
+      result[i] = h
+      h -= 1
+    end
+  end
+
+  result[n] = l
+
+  result
+end
