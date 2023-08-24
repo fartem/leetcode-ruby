9cfd2723bc12025fcbce50103b16a940a7b659e0
fartem <jaman.smlnsk@gmail.com>
2023-08-24 09:46:53 +0300

    2023-08-24 v. 1.8.1: added "953. Verifying an Alien Dictionary"

---
+++ lib/easy/953_verifying_an_alien_dictionary.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/verifying-an-alien-dictionary/
+# @param {String[]} words
+# @param {String} order
+# @return {Boolean}
+def is_alien_sorted(words, order)
+  dictionary = ::Array.new(128)
+  dictionary['@'.ord] = -1
+  (0...order.length).step(1) do |i|
+    dictionary[order[i].ord] = i
+  end
+
+  (1...words.length).step(1) do |i|
+    f = words[i - 1]
+    s = words[i]
+    f_l = f.length
+    s_l = s.length
+    max_l = [f_l, s_l].max
+    (0...max_l).step(1) do |j|
+      a = f_l <= j ? '@' : f[j]
+      b = s_l <= j ? '@' : s[j]
+
+      return false if dictionary[a.ord] > dictionary[b.ord]
+
+      break if a != b
+    end
+  end
+
+  true
+end
