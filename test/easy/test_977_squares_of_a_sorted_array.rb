1a0a0344014966b3810da2d48226436f837ffd3e
fartem <jaman.smlnsk@gmail.com>
2023-08-25 06:33:57 +0300

    2023-08-25 v. 1.8.5: added "977. Squares of a Sorted Array"

---
+++ test/easy/test_977_squares_of_a_sorted_array.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/easy/977_squares_of_a_sorted_array'
+require 'minitest/autorun'
+
+class SquaresOfASortedArrayTest < ::Minitest::Test
+  def test_default
+    assert_equal([0, 1, 9, 16, 100], sorted_squares([-4, -1, 0, 3, 10]))
+    assert_equal([4, 9, 9, 49, 121], sorted_squares([-7, -3, 2, 3, 11]))
+  end
+end
