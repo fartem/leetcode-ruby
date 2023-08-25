b3d0179c5ac3f39cc66ca88be45ce7cc97ddfb99
fartem <jaman.smlnsk@gmail.com>
2023-08-25 12:18:22 +0300

    2023-08-25 v. 1.9.4: added "1022. Sum of Root To Leaf Binary Numbers"

---
+++ test/easy/test_1022_sum_of_root_to_leaf_binary_numbers.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/common/binary_tree'
+require_relative '../../lib/easy/1022_sum_of_root_to_leaf_binary_numbers'
+require 'minitest/autorun'
+
+class SumOfRootToLeafBinaryNumbersTest < ::Minitest::Test
+  def test_default
+    assert_equal(
+      22,
+      sum_root_to_leaf(
+        ::TreeNode.new(
+          1,
+          ::TreeNode.new(
+            0,
+            ::TreeNode.new(0),
+            ::TreeNode.new(1)
+          ),
+          ::TreeNode.new(
+            1,
+            ::TreeNode.new(0),
+            ::TreeNode.new(1)
+          )
+        )
+      )
+    )
+    assert_equal(
+      0,
+      sum_root_to_leaf(
+        ::TreeNode.new(0)
+      )
+    )
+  end
+end
