ee66a58cb8933c7249f3d215ff2fb0e4541e22db
fartem <jaman.smlnsk@gmail.com>
2023-08-27 13:12:10 +0300

    2023-08-27 v. 2.0.5: updated "219. Contains Duplicate II"

--- lib/easy/219_contains_duplicate_ii.rb
+++ lib/easy/219_contains_duplicate_ii.rb
@@ -6,12 +6,12 @@
 # @return {Boolean}
 def contains_nearby_duplicate(nums, k)
   nums_with_indices = {}
-  (0..nums.length).step(1) do |i|
+  (0..nums.length).each do |i|
     num = nums[i]
     if nums_with_indices.include?(num)
       indices = nums_with_indices[num]
       indices << i
-      (0...indices.length - 1).step(1) do |j|
+      (0...indices.length - 1).each do |j|
         return true if (indices[j] - indices[j + 1]).abs <= k
       end
 
