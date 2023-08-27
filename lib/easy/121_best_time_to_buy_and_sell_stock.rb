7abd5c3976f13f7a091be47cd78409f7861baab1
fartem <jaman.smlnsk@gmail.com>
2023-08-27 12:52:39 +0300

    2023-08-27 v. 1.9.8: updated "121. Best Time to Buy and Sell Stock"

--- lib/easy/121_best_time_to_buy_and_sell_stock.rb
+++ lib/easy/121_best_time_to_buy_and_sell_stock.rb
@@ -6,7 +6,7 @@
 def max_profit(prices)
   min = prices.first
   max = 0
-  (1...prices.length).step(1) do |i|
+  (1...prices.length).each do |i|
     price = prices[i]
     max = [price - min, max].max
     min = [price, min].min
