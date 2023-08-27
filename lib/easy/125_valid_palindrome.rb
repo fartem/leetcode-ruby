e0cb75cb485872f0fc502bfd2ba456fbce3c34f2
fartem <jaman.smlnsk@gmail.com>
2023-08-27 12:55:11 +0300

    2023-08-27 v. 1.9.9: updated "125. Valid Palindrome"

--- lib/easy/125_valid_palindrome.rb
+++ lib/easy/125_valid_palindrome.rb
@@ -7,7 +7,7 @@ def is_valid_palindrome(s)
   prepared_str = s.downcase.delete(' ').gsub(/[^0-9a-z ]/i, '')
   p = prepared_str.length - 1
   result = true
-  (0...prepared_str.length).step(1) do |i|
+  (0...prepared_str.length).each do |i|
     if prepared_str[i] == prepared_str[p]
       p -= 1
     else
