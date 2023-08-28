72d3828b8cb9357069e12e0e32e65e7afae986cb
fartem <jaman.smlnsk@gmail.com>
2023-08-28 08:35:35 +0300

    2023-08-28 v. 2.3.0: updated "821. Shortest Distance to a Character"

--- lib/easy/821_shortest_distance_to_a_character.rb
+++ lib/easy/821_shortest_distance_to_a_character.rb
@@ -8,12 +8,12 @@ def shortest_to_char(s, c)
   result = ::Array.new(s.length, '')
 
   indexes = []
-  (0...s.length).step(1) do |i|
+  (0...s.length).each do |i|
     indexes << i if s[i] == c
   end
 
   pos = 0
-  (0...s.length).step(1) do |i|
+  (0...s.length).each do |i|
     index = indexes[pos]
     first = (index - i).abs
 
