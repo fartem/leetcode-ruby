7507dbc474ce1679b7c0066c638f6833b1363374
fartem <jaman.smlnsk@gmail.com>
2023-08-29 10:39:54 +0300

    2023-08-29 v. 2.5.2: updated "868. Binary Gap"

--- lib/easy/868_binary_gap.rb
+++ lib/easy/868_binary_gap.rb
@@ -8,7 +8,7 @@ def binary_gap(n)
   distance = 0
   count = 0
   is_counting = false
-  (0...binary.length).step(1) do |i|
+  (0...binary.length).each do |i|
     if binary[i] == '1'
       if is_counting
         count += 1
