69fdc9de669c29c75778246eda7b50a8628825cb
fartem <jaman.smlnsk@gmail.com>
2023-08-29 08:53:29 +0300

    2023-08-29 v. 2.4.3: updated "746. Min Cost Climbing Stairs"

--- lib/easy/746_min_cost_climbing_stairs.rb
+++ lib/easy/746_min_cost_climbing_stairs.rb
@@ -10,7 +10,7 @@ def min_cost_climbing_stairs(cost)
 
   dp = ::Array.new(n + 1, 0)
   dp_n = dp.length
-  (2...dp_n).step(1) do |i|
+  (2...dp_n).each do |i|
     dp[i] = [dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]].min
   end
 
