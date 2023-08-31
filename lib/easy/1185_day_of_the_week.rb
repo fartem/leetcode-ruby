19dde74995a7c913838bc13fb1a19e3b552e851e
fartem <jaman.smlnsk@gmail.com>
2023-08-31 08:52:01 +0300

    2023-08-31 v. 2.6.7: added "1185. Day of the Week"

---
+++ lib/easy/1185_day_of_the_week.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+$days_of_week = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday]
+$days_in_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+# https://leetcode.com/problems/day-of-the-week/
+# @param {Integer} day
+# @param {Integer} month
+# @param {Integer} year
+# @return {String}
+def day_of_the_week(day, month, year)
+  (1971...year).each do |i|
+    day += is_leap?(i) ? 366 : 365
+  end
+
+  day += 1 if month > 2 && is_leap?(year)
+
+  (1...month).each do |i|
+    day += $days_in_month[i]
+  end
+
+  $days_of_week[(day + 4) % 7]
+end
+
+# @param {Integer} year
+# @return {Boolean}
+def is_leap?(year)
+  ((year % 4).zero? && (year % 100).positive?) || (year % 400).zero?
+end
