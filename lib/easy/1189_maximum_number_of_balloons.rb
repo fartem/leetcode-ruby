3fabc2784c8156864e6d33f6274024743c826ee0
fartem <jaman.smlnsk@gmail.com>
2023-08-31 09:03:26 +0300

    2023-08-31 v. 2.6.8: added "1189. Maximum Number of Balloons"

---
+++ lib/easy/1189_maximum_number_of_balloons.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# @param {String} text
+# @return {Integer}
+def max_number_of_balloons(text)
+  count = ::Array.new(128, 0)
+  (0...text.length).each do |i|
+    count[text[i].ord] += 1
+  end
+
+  result = 1_000_000_000_000
+  (0...count.length).each do |i|
+    c = i.chr
+    value = 1_000_000_000_000
+    case c
+    when 'a', 'b', 'n'
+      value = count[i]
+    when 'l', 'o'
+      value = count[i] / 2
+    end
+
+    return 0 if value.zero?
+
+    result = value if value < result
+  end
+
+  result
+end
