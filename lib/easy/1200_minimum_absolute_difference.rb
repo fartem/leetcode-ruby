551e425272e9b3a497cf62fed518c2853743c51a
fartem <jaman.smlnsk@gmail.com>
2023-09-01 06:34:25 +0300

    2023-09-01 v. 2.6.9: added "1200. Minimum Absolute Difference"

---
+++ lib/easy/1200_minimum_absolute_difference.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/minimum-absolute-difference/
+# @param {Integer[]} arr
+# @return {Integer[][]}
+def minimum_abs_difference(arr)
+  arr.sort!
+  diff = 1_000_000_000_000
+  (0...(arr.length - 1)).each do |i|
+    abs = (arr[i] - arr[i + 1]).abs
+    diff = abs if diff > abs
+  end
+
+  result = []
+  (0...(arr.length - 1)).each do |i|
+    f = arr[i]
+    s = arr[i + 1]
+    result << [f, s].minmax if (f - s).abs == diff
+  end
+
+  result
+end
