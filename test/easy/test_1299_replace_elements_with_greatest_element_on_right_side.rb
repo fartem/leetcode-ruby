88a9ed72794bfb315b95b75e2e3b41115faefbd0
fartem <jaman.smlnsk@gmail.com>
2023-09-06 07:35:11 +0300

    2023-09-06 v. 2.8.1: added "1299. Replace Elements with Greatest Element on Right Side"

---
+++ test/easy/test_1299_replace_elements_with_greatest_element_on_right_side.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/easy/1299_replace_elements_with_greatest_element_on_right_side'
+require 'minitest/autorun'
+
+class ReplaceElementsWithGreatestElementOnRightSideTest < ::Minitest::Test
+  def test_default
+    assert_equal([18, 6, 6, 6, 1, -1], replace_elements([17, 18, 5, 4, 6, 1]))
+    assert_equal([-1], replace_elements([400]))
+  end
+end
