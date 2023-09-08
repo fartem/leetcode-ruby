0510771376ba20c2c7615d04ee786a46f0c09f77
fartem <jaman.smlnsk@gmail.com>
2023-09-08 07:46:54 +0300

    2023-09-08 v. 2.8.4: added "1313. Decompress Run-Length Encoded List"

---
+++ test/easy/test_1313_decompress_run_length_encoded_list.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/easy/1313_decompress_run_length_encoded_list'
+require 'minitest/autorun'
+
+class DecompressRunLengthEncodedListTest < ::Minitest::Test
+  def test_default
+    assert_equal([2, 4, 4, 4], decompress_rl_elist([1, 2, 3, 4]))
+    assert_equal([1, 3, 3], decompress_rl_elist([1, 1, 2, 3]))
+  end
+end
