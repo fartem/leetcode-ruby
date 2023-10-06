302e03770a10426fe2546da75379f6b2904e095a
fartem <jaman.smlnsk@gmail.com>
2023-10-06 09:49:30 +0300

    2023-10-06 v. 3.2.0: added "1507. Reformat Date"

---
+++ lib/easy/1507_reformat_date.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+$months = {
+  'Jan' => '01',
+  'Feb' => '02',
+  'Mar' => '03',
+  'Apr' => '04',
+  'May' => '05',
+  'Jun' => '06',
+  'Jul' => '07',
+  'Aug' => '08',
+  'Sep' => '09',
+  'Oct' => '10',
+  'Nov' => '11',
+  'Dec' => '12'
+}
+
+# @param {String} date
+# @return {String}
+def reformat_date(date)
+  length = date.length
+
+  result = []
+  result << date[length - 4, length]
+  result << '-'
+
+  space = date.index(' ')
+
+  result << $months[date[space, space + 1].strip]
+  result << '-'
+  result << if space == 4
+              date[0, 2]
+            else
+              "0#{date[0, 1]}"
+            end
+
+  result.join
+end
---
+++ test/easy/test_1507_reformat_date.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/easy/1507_reformat_date'
+require 'minitest/autorun'
+
+class ReformatDateTest < ::Minitest::Test
+  def test_default
+    assert_equal('2052-10-20', reformat_date('20th Oct 2052'))
+    assert_equal('1933-06-06', reformat_date('6th Jun 1933'))
+    assert_equal('1960-05-26', reformat_date('26th May 1960'))
+  end
+end
