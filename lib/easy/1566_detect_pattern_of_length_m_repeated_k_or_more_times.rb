50308d63f0f62c983605136a5f318cc23b5f12fd
fartem <jaman.smlnsk@gmail.com>
2023-10-14 11:40:51 +0300

    2023-10-14 v. 3.3.0: added "1566. Detect Pattern of Length M Repeated K or More Times"

--- lib/easy/1566_detect_pattern_of_length_m_repeated_k_or_more_times.rb
+++ lib/easy/1566_detect_pattern_of_length_m_repeated_k_or_more_times.rb
@@ -45,29 +45,3 @@ def contains_pattern(arr, m, k)
 
   false
 end
-
-#         for (int i = 0; i < arr.length - m; i++) {
-#             int[] pattern = new int[m];
-#             for (int j = i, l = 0; l < m; j++, l++) {
-#                 pattern[l] = arr[j];
-#             }
-#             int count = 1;
-#             for (int j = i + m; j <= arr.length - m; j += m) {
-#                 int length = 0;
-#                 for (int l = j, p = 0; p < m; l++, p++) {
-#                     if (pattern[p] != arr[l]) {
-#                         break;
-#                     }
-#                     length++;
-#                 }
-#                 if (length >= m && length % m == 0) {
-#                     count += length / m;
-#                 } else {
-#                     break;
-#                 }
-#             }
-#             if (count == k) {
-#                 return true;
-#             }
-#         }
-#         return false;
