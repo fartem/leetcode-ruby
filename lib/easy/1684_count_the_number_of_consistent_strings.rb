bde94b495ba6e679ebac67a7498fa1d9fb206037
fartem <jaman.smlnsk@gmail.com>
2023-11-10 12:05:21 +0300

    2023-11-10 v. 3.5.0: added "1684. Count the Number of Consistent Strings"

---
+++ lib/easy/1684_count_the_number_of_consistent_strings.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'set'
+
+# @param {String} allowed
+# @param {String[]} words
+# @return {Integer}
+def count_consistent_strings(allowed, words)
+  allowed_chars = allowed.split('').to_set
+  result = words.length
+  words.each do |word|
+    (0...word.length).each do |i|
+      next if allowed_chars.include?(word[i])
+
+      result -= 1
+
+      break
+    end
+  end
+
+  result
+end
