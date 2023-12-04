a3b6639b2159e1e1809c7d35c07c9bb36e678b5a
fartem <jaman.smlnsk@gmail.com>
2023-12-04 08:04:20 +0300

    2023-12-04 v. 3.6.6: added "1784. Check if Binary String Has at Most One Segment of Ones"

---
+++ test/easy/test_1784_check_if_binary_string_has_at_most_one_segment_of_ones.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/easy/1784_check_if_binary_string_has_at_most_one_segment_of_ones'
+require 'minitest/autorun'
+
+class CheckIfBinaryStringHasAtMostOneSegmentOfOnesTest < ::Minitest::Test
+  def test_default
+    assert(!check_ones_segment('1001'))
+    assert(check_ones_segment('110'))
+  end
+end
