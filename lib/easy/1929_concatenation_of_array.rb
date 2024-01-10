4998075801a76bf8e371e511b2ce046f3353dd21
fartem <jaman.smlnsk@gmail.com>
2024-01-10 07:43:39 +0300

    2024-01-10 v. 3.9.3: added "1929. Concatenation of Array"

---
+++ lib/easy/1929_concatenation_of_array.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/concatenation-of-array/description/
+# @param {Integer[]} nums
+# @return {Integer[]}
+def get_concatenation(nums)
+  nums + nums
+end
