2c74c61ebe55cc9e93ccdcf023492db2277a26f8
fartem <jaman.smlnsk@gmail.com>
2024-02-01 08:48:34 +0300

    2024-02-01 v. 4.3.2: updated "1897. Redistribute Characters to Make All Strings Equal"

--- lib/easy/1897_redistribute_characters_to_make_all_strings_equal.rb
+++ lib/easy/1897_redistribute_characters_to_make_all_strings_equal.rb
@@ -4,10 +4,10 @@
 # @param {String[]} words
 # @return {Boolean}
 def make_equal(words)
-  letters = ::Array.new(26, 0)
+  letters = ::Array.new(128, 0)
   words.each do |word|
-    (0...word.length).each do |i|
-      letters[word[i].ord - 97] += 1
+    word.each_byte do |b|
+      letters[b] += 1
     end
   end
 
