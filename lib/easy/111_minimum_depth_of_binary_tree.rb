a89254e406c07e3a9ca1824027e7a6cc9384a9ef
fartem <jaman.smlnsk@gmail.com>
2024-02-10 08:47:05 +0300

    2024-02-10 v. 4.5.9: updated "101. Symmetric Tree"

--- lib/easy/111_minimum_depth_of_binary_tree.rb
+++ lib/easy/111_minimum_depth_of_binary_tree.rb
@@ -5,8 +5,11 @@
 # @return {Integer}
 def min_depth(root)
   return 0 if root.nil?
+
   return 1 if root.left.nil? && root.right.nil?
+
   return min_depth(root.right) + 1 if root.left.nil?
+
   return min_depth(root.left) + 1 if root.right.nil?
 
   [min_depth(root.left) + 1, min_depth(root.right) + 1].min
