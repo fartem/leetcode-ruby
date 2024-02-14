4ceb41c9182941c4416ee3fa5313f9b7f3d27ad0
fartem <jaman.smlnsk@gmail.com>
2024-02-14 08:53:34 +0300

    2024-02-14 v. 4.7.7: updated "387. First Unique Character in a String"

--- lib/easy/387_first_unique_character_in_a_string.rb
+++ lib/easy/387_first_unique_character_in_a_string.rb
@@ -5,12 +5,8 @@
 # @return {Integer}
 def first_uniq_char(s)
   chars = ::Hash.new(0)
-  (0..s.length).each do |i|
-    chars[s[i]] += 1
-  end
-  (0...s.length).each do |i|
-    return i if chars[s[i]] == 1
-  end
+  s.each_char { |c| chars[c] += 1 }
+  (0...s.length).each { |i| return i if chars[s[i]] == 1 }
 
   -1
 end
