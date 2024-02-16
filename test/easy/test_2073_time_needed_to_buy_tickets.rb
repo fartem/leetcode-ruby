56949074f9eac115840c9a5391f990f1a625ca86
fartem <jaman.smlnsk@gmail.com>
2024-02-16 07:31:12 +0300

    2024-02-16 v. 4.8.5: added "2073. Time Needed to Buy Tickets"

---
+++ test/easy/test_2073_time_needed_to_buy_tickets.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/easy/2073_time_needed_to_buy_tickets'
+require 'minitest/autorun'
+
+class TimeNeededToBuyTicketsTest < ::Minitest::Test
+  def test_default
+    assert_equal(6, time_required_to_buy([2, 3, 2], 2))
+    assert_equal(8, time_required_to_buy([5, 1, 1, 1], 0))
+  end
+end
