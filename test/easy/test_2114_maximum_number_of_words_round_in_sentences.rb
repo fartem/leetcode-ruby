b407f42e6ea3725cd14655da9fc85e94577e8d4c
fartem <jaman.smlnsk@gmail.com>
2024-02-28 08:19:03 +0300

    2024-02-28 v. 5.1.4: added "2114. Maximum Number of Words Found in Sentences"

---
+++ test/easy/test_2114_maximum_number_of_words_round_in_sentences.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/easy/2114_maximum_number_of_words_round_in_sentences'
+require 'minitest/autorun'
+
+class MaximumNumberOfWordsRoundInSentencesTest < ::Minitest::Test
+  def test_default
+    assert_equal(
+      6,
+      most_words_found(['alice and bob love leetcode', 'i think so too', 'this is great thanks very much'])
+    )
+    assert_equal(3, most_words_found(['please wait', 'continue to fight', 'continue to win']))
+  end
+end
