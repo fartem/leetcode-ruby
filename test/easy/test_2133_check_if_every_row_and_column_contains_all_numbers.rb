8b1b035eec7c0b562267eaa31714eaf0b962e65b
fartem <jaman.smlnsk@gmail.com>
2024-03-05 08:26:26 +0300

    2024-03-05 v. 5.1.8: added "2133. Check if Every Row and Column Contains All Numbers"

---
+++ test/easy/test_2133_check_if_every_row_and_column_contains_all_numbers.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/easy/2133_check_if_every_row_and_column_contains_all_numbers'
+require 'minitest/autorun'
+
+class CheckIfEveryRowAndColumnContainsAllNumbersTest < ::Minitest::Test
+  def test_default
+    assert(check_valid([[1, 2, 3], [3, 1, 2], [2, 3, 1]]))
+    assert(!check_valid([[1, 1, 1], [1, 2, 3], [1, 2, 3]]))
+  end
+end
