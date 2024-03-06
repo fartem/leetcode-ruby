1f07ffbb7b2d8fd5d2f4f700af7d85732bdc9538
fartem <jaman.smlnsk@gmail.com>
2024-03-06 09:26:44 +0300

    2024-03-05 v. 5.1.8: added "2133. Check if Every Row and Column Contains All Numbers"

---
+++ test/easy/test_2138_divide_a_string_into_groups_of_size_k.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/easy/2138_divide_a_string_into_groups_of_size_k'
+require 'minitest/autorun'
+
+class DivideAStringIntoGroupsOfSizeKTest < ::Minitest::Test
+  def test_default
+    assert_equal(%w[abc def ghi], divide_string('abcdefghi', 3, 'x'))
+    assert_equal(%w[abc def ghi jxx], divide_string('abcdefghij', 3, 'x'))
+  end
+end
