7bda7c198cb2334271c5f32837346235dab00201
fartem <jaman.smlnsk@gmail.com>
2024-03-29 09:53:40 +0300

    2024-03-29 v. 5.3.5: added "2235. Add Two Integers"

---
+++ lib/easy/2235_add_two_integers.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/add-two-integers/
+# @param {Integer} num1
+# @param {Integer} num2
+# @return {Integer}
+def sum(num1, num2)
+  num1 + num2
+end
