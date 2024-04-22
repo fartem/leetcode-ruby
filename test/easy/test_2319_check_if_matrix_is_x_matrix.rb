dad2032e5995b541cf2b21956f1f4ee8120e1e22
fartem <jaman.smlnsk@gmail.com>
2024-04-22 10:04:25 +0300

    2024-04-22 v. 5.5.1: added "2319. Check if Matrix Is X-Matrix"

---
+++ test/easy/test_2319_check_if_matrix_is_x_matrix.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/easy/2319_check_if_matrix_is_x_matrix'
+require 'minitest/autorun'
+
+class CheckIfMatrixIsXMatrixTest < ::Minitest::Test
+  def test_default
+    assert(check_x_matrix([[2, 0, 0, 1], [0, 3, 1, 0], [0, 5, 2, 0], [4, 0, 0, 2]]))
+    assert(!check_x_matrix([[5, 7, 0], [0, 3, 1], [0, 5, 0]]))
+  end
+end
