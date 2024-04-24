92ed5f136d39c696efa59e9db2e3382cd9a97c0c
fartem <jaman.smlnsk@gmail.com>
2024-04-24 08:44:36 +0300

    2024-04-24 v. 5.5.3: added "2331. Evaluate Boolean Binary Tree"

---
+++ test/easy/test_2331_evaluate_boolean_binary_tree.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/easy/2331_evaluate_boolean_binary_tree'
+require_relative '../../lib/common/binary_tree'
+require 'minitest/autorun'
+
+class EvaluateBooleanBinaryTreeTest < ::Minitest::Test
+  def test_default
+    assert(
+      evaluate_tree(
+        ::TreeNode.new(
+          2,
+          ::TreeNode.new(1),
+          ::TreeNode.new(
+            3,
+            ::TreeNode.new(0),
+            ::TreeNode.new(1)
+          )
+        )
+      )
+    )
+    assert(
+      !evaluate_tree(
+        ::TreeNode.new(0)
+      )
+    )
+  end
+
+  def test_additional
+    assert(
+      !evaluate_tree(
+        ::TreeNode.new(
+          3,
+          ::TreeNode.new(0),
+          ::TreeNode.new(1)
+        )
+      )
+    )
+  end
+end
