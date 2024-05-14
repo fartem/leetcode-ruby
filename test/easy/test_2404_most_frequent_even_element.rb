47d4a6d6311fec466fec6608458d38ad0a5f3679
fartem <jaman.smlnsk@gmail.com>
2024-05-14 07:45:33 +0300

    2024-05-13 v. 5.6.7: added "2404. Most Frequent Even Element"

---
+++ test/easy/test_2404_most_frequent_even_element.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/easy/2404_most_frequent_even_element'
+require 'minitest/autorun'
+
+class MostFrequentEvenElementTest < ::Minitest::Test
+  def test_default
+    assert_equal(2, most_frequent_even([0, 1, 2, 2, 4, 4, 1]))
+    assert_equal(4, most_frequent_even([4, 4, 4, 9, 2, 4]))
+    assert_equal(-1, most_frequent_even([29, 47, 21, 41, 13, 37, 25, 7]))
+  end
+end
