1f90361a831342509992e785780fc5c3a8fb84df
fartem <jaman.smlnsk@gmail.com>
2024-05-27 09:23:47 +0300

    2024-05-27 v. 5.7.5: added "2455. Average Value of Even Numbers That Are Divisible by Three"

---
+++ test/easy/test_2455_average_value_of_even_numbers_that_are_divisible_by_three.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/easy/2455_average_value_of_even_numbers_that_are_divisible_by_three'
+require 'minitest/autorun'
+
+class AverageValueOfEvenNumbersThatAreDivisibleByThreeTest < ::Minitest::Test
+  def test_default
+    assert_equal(9, average_value([1, 3, 6, 10, 12, 15]))
+    assert_equal(0, average_value([1, 2, 4, 7, 10]))
+  end
+end
