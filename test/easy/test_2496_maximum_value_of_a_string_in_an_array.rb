5b772dc1ba26e7f45283ec00257ca747249876bc
fartem <jaman.smlnsk@gmail.com>
2024-06-05 09:43:45 +0300

    2024-06-05 v. 5.8.2: added "2496. Maximum Value of a String in an Array"

---
+++ test/easy/test_2496_maximum_value_of_a_string_in_an_array.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/easy/2496_maximum_value_of_a_string_in_an_array'
+require 'minitest/autorun'
+
+class MaximumValueOfAStringInAnArrayTest < ::Minitest::Test
+  def test_default
+    assert_equal(5, maximum_value(%w[alic3 bob 3 4 00000]))
+    assert_equal(1, maximum_value(%w[1 01 001 0001]))
+  end
+end
