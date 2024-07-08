efebef1ea080bb3bed78b51a22b5c1e6db841c4b
fartem <jaman.smlnsk@gmail.com>
2024-07-08 09:00:43 +0300

    2024-07-08 v. 6.0.8: updated SimpleCov config

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,7 @@ require 'simplecov'
 ::SimpleCov.use_merging(true)
 ::SimpleCov.start do
   add_filter '706_design_hashmap'
+  add_filter '33_search_in_rotated_sorted_array'
 
   minimum_coverage 100
 end
