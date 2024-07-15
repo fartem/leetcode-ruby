642517d712e364c204c46b87a3ce65c789397660
fartem <jaman.smlnsk@gmail.com>
2024-07-15 06:27:01 +0300

    2024-07-15 v. 6.1.5: added "48. Rotate Image"

---
+++ test/medium/test_48_rotate_image.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/medium/48_rotate_image'
+require 'minitest/autorun'
+
+class RotateImageTest < ::Minitest::Test
+  def test_default
+    matrix = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ]
+    rotate(matrix)
+
+    assert_equal(
+      [
+        [7, 4, 1],
+        [8, 5, 2],
+        [9, 6, 3]
+      ],
+      matrix
+    )
+
+    matrix = [
+      [5, 1, 9, 11],
+      [2, 4, 8, 10],
+      [13, 3, 6, 7],
+      [15, 14, 12, 16]
+    ]
+    rotate(matrix)
+
+    assert_equal(
+      [
+        [15, 13, 2, 5],
+        [14, 3, 4, 1],
+        [12, 6, 8, 9],
+        [16, 7, 10, 11]
+      ],
+      matrix
+    )
+  end
+end
