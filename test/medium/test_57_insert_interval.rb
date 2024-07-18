ef60d6a5648d5d145a4db4d3b62a093e5c3340a7
fartem <jaman.smlnsk@gmail.com>
2024-07-18 08:03:54 +0300

    2024-07-18 v. 6.2.1: added "57. Insert Interval"

---
+++ test/medium/test_57_insert_interval.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/medium/57_insert_interval'
+require 'minitest/autorun'
+
+class InsertIntervalTest < ::Minitest::Test
+  def test_default
+    assert_equal([[1, 5], [6, 9]], insert([[1, 3], [6, 9]], [2, 5]))
+    assert_equal([[1, 2], [3, 10], [12, 16]], insert([[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], [4, 8]))
+  end
+end
