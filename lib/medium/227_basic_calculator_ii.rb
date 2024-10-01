3dda2671d470c624dd946c8929796e8b1b5534f8
fartem <jaman.smlnsk@gmail.com>
2024-10-01 08:56:52 +0300

    2024-10-01 v. 6.7.2: added "227. Basic Calculator II"

---
+++ lib/medium/227_basic_calculator_ii.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+# @param {String} s
+# @return {Integer}
+def calculate(s)
+  stack = []
+  curr = 0
+  op = '+'
+
+  s.each_char do |c|
+    if c.match?(/[0-9]/)
+      curr = curr * 10 + c.to_i
+    elsif c != ' '
+      stack.push(
+        perform_operation(
+          stack,
+          op,
+          curr
+        )
+      )
+
+      curr = 0
+      op = c
+    end
+  end
+
+  stack.push(
+    perform_operation(
+      stack,
+      op,
+      curr
+    )
+  )
+
+  stack.sum
+end
+
+private
+
+# @param {Array} stack
+# @param {String} op
+# @param {Integer} num
+# @return {Integer}
+def perform_operation(stack, op, num)
+  case op
+  when '+'
+    num
+  when '-'
+    -num
+  when '*'
+    stack.pop * num
+  when '/'
+    (stack.pop.to_f / num).to_i
+  end
+end
