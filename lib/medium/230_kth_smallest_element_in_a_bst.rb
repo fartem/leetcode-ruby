7459d4a958381dfa71a3d34be0110a2a03d39cd2
fartem <jaman.smlnsk@gmail.com>
2024-10-02 06:28:52 +0300

    2024-10-02 v. 6.7.3: added "230. Kth Smallest Element in a BST"

---
+++ lib/medium/230_kth_smallest_element_in_a_bst.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# @param {TreeNode} root
+# @param {Integer} k
+# @return {Integer}
+def kth_smallest(root, k)
+  nums = []
+  apply230(root, nums)
+
+  nums[k - 1]
+end
+
+private
+
+# @param {TreeNode} root
+# @param {Array} nums
+# @return {Void}
+def apply230(node, nums)
+  return unless node
+
+  apply230(node.left, nums)
+  nums << node.val
+  apply230(node.right, nums)
+end
