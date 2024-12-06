813f60755c416b3e742ba5e604926e4d1c77fa76
fartem <jaman.smlnsk@gmail.com>
2024-12-06 14:49:49 +0300

    2024-12-06 v. 7.2.5: added "535. Encode and Decode TinyURL"

---
+++ lib/medium/535_encode_and_decode_tinyurl.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+$urls = {}
+
+# https://leetcode.com/problems/encode-and-decode-tinyurl/
+# @param {String} long_url
+# @return {String}
+def encode(long_url)
+  short_url = generate_short_url(long_url)
+  short_url = generate_short_url(long_url) while $urls.include?(short_url)
+
+  $urls[short_url] = long_url
+
+  short_url
+end
+
+# @param {String} long_url
+# @return {String}
+def generate_short_url(_long_url)
+  symbols = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+  short_url = []
+  (0...6).each { |_| short_url << symbols[rand(symbols.size)] }
+
+  short_url.join
+end
+
+# @param {String} short_url
+# @return {String}
+def decode(short_url) = $urls[short_url]
