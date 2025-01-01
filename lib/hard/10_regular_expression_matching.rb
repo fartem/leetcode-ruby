971b2293fb5bb17bfe2447574aac15cdc2b5c5a0
fartem <jaman.smlnsk@gmail.com>
2025-01-01 03:22:48 +0300

    2025-01-01 v. 7.6.1: updated "10. Regular Expression Matching"

--- lib/hard/10_regular_expression_matching.rb
+++ lib/hard/10_regular_expression_matching.rb
@@ -9,13 +9,13 @@
 def is_match(s, p)
   s = s.bytes
   p = p.bytes
-  dp = ::Array.new(s.length + 1) { ::Array.new(p.length + 1, false) }
-  m = s.length
-  n = p.length
+  dp = ::Array.new(s.size + 1) { ::Array.new(p.size + 1, false) }
+  m = s.size
+  n = p.size
   dp[m][n] = true
 
   (0..m).reverse_each do |i|
-    (n - 1).downto(0) do |j|
+    (0...n).reverse_each do |j|
       first = i < m && (s[i] == p[j] || p[j] == 46)
       dp[i][j] =
         if p[j + 1] == 42
