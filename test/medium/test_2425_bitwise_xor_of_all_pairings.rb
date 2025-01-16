189a8b50bab547347549eebd42e95f51a57ebc07
fartem <jaman.smlnsk@gmail.com>
2025-01-16 08:05:49 +0300

    2025-01-16 v. 8.1.0: added "2425. Bitwise XOR of All Pairings"

---
+++ test/medium/test_2425_bitwise_xor_of_all_pairings.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../lib/medium/2425_bitwise_xor_of_all_pairings'
+require 'minitest/autorun'
+
+class MinimizeXORTest < ::Minitest::Test
+  def test_default_one
+    assert_equal(
+      13,
+      xor_all_nums(
+        [2, 1, 3],
+        [10, 2, 5, 0]
+      )
+    )
+  end
+
+  def test_default_two
+    assert_equal(
+      0,
+      xor_all_nums(
+        [1, 2],
+        [3, 4]
+      )
+    )
+  end
+end
