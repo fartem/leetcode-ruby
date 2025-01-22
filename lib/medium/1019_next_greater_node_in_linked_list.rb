35c842b5f3ca25723ccd37a437e8cb41ea92d58e
fartem <jaman.smlnsk@gmail.com>
2025-01-22 11:47:46 +0300

    2025-01-22 v. 8.2.3: added "1019. Next Greater Node In Linked List"

---
+++ lib/medium/1019_next_greater_node_in_linked_list.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/next-greater-node-in-linked-list/
+# @param {ListNode} head
+# @return {Integer[]}
+def next_larger_nodes(head)
+  nodes = []
+  pointer = head
+
+  while pointer
+    nodes << pointer.val
+    pointer = pointer.next
+  end
+
+  result = ::Array.new(nodes.size, 0)
+  values = []
+
+  nodes.each_with_index do |node, i|
+    result[values.pop] = node while !values.empty? && node > nodes[values.last]
+    values.push(i)
+  end
+
+  result
+end
