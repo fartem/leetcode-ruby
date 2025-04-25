e5af5b7b4ff8157ab2d03dfcd73c04cc4ab90577
fartem <jaman.smlnsk@gmail.com>
2025-04-25 12:34:27 +0300

    2025-04-25 v. 9.3.4.1: updated StageChecker

--- ci/stage_checker.rb
+++ ci/stage_checker.rb
@@ -3,16 +3,10 @@
 require_relative './ci_job'
 
 module CI
-  # CI job that checks all files are staged for commit.
+  # CI job that add all files to stage for commit.
   class StageChecker < ::CI::CIJob
     # Process StageChecker.
     # @return {Void}
-    def process
-      git_status = `git status --porcelain`
-
-      return if git_status.empty?
-
-      end_with_error(-> { puts("StageChecker found unstaged changes:\n#{git_status}") })
-    end
+    def process = `git add -u`
   end
 end
